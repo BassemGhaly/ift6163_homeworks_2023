0e44daf8487399edfa0c9a73933368bebf8d3798
bassem <bassem.michel97@gmail.com>
2023-04-19 22:47:08 -0400

    Ros on conda and turtlebot3 with Rgb-d camera, and rviz launch file, added github submodules as forlders

---
+++ project/catkin_ws/devel/share/turtlebot3_example/cmake/turtlebot3_example-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(turtlebot3_example_MSG_INCLUDE_DIRS "/home/bassem/RobotLearning/ift6163_homeworks_2023/project/catkin_ws/devel/share/turtlebot3_example/msg")
+set(turtlebot3_example_MSG_DEPENDENCIES std_msgs;geometry_msgs;actionlib_msgs)
