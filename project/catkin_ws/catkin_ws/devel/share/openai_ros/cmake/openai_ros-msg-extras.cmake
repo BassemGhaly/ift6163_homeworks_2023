3de2945a619c93b65699ba72effcafad656b7798
Bassem Ghaly <bassem.michel97@gmail.com>
2023-04-20 14:27:07 -0400

    Ros on conda, turtlebot3 rgb-d camera, rviz launch file, no submodules

---
+++ project/catkin_ws/catkin_ws/devel/share/openai_ros/cmake/openai_ros-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(openai_ros_MESSAGE_FILES "/home/bassem/RobotLearning/ift6163_homeworks_2023/project/catkin_ws/src/openai_ros/openai_ros/msg/RLExperimentInfo.msg")
+set(openai_ros_SERVICE_FILES "")
