ac3b6c22142392e07a5bde9efec3b6c4edb2b27d
Bassem Ghaly <bassem.michel97@gmail.com>
2023-04-20 14:24:08 -0400

    delete catkin_ws

--- project/catkin_ws/devel/share/turtlebot3_msgs/cmake/turtlebot3_msgs-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(turtlebot3_msgs_MSG_INCLUDE_DIRS "/home/bassem/RobotLearning/ift6163_homeworks_2023/project/catkin_ws/src/turtlebot3_msgs/msg")
-set(turtlebot3_msgs_MSG_DEPENDENCIES std_msgs)
